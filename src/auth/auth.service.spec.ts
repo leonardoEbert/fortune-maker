92ff827acd64ea69dd5d912f157e4907c8472b19
leonardo <leoebert19@gmail.com>
2024-04-11 22:37:41 -0300

    test: unit tests initial implementations and configs

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserModule } from '@/user/user.module';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [UserModule],
       providers: [AuthService],
     }).compile();
 
