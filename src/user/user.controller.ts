164ecd0ce0c33d9fd755b66ca36af99ea8fa77a9
leonardo <leoebert19@gmail.com>
2024-04-12 22:05:03 -0300

    lint: fixed eslint error

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UserService } from '@/user/user.service';
 import { CreateUserDto } from '@/user/dto/create-user.dto';
 import { User } from '@/user/user.entity';
