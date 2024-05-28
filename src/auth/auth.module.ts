1237bf0d46a00fc6a2115c7458c9562117d7dc89
Leonardo Ebert <leoebert19@gmail.com>
2024-05-28 15:54:36 -0300

    feat: added casl package and created module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from '@/auth/auth.guard';
+import { CaslModule } from './casl/casl.module';
 
 @Module({
   controllers: [AuthController],
@@ -30,6 +31,7 @@ import { AuthGuard } from '@/auth/auth.guard';
       }),
       inject: [ConfigService],
     }),
+    CaslModule,
   ],
 })
 export class AuthModule {}
