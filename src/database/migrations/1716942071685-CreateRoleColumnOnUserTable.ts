2487f13e01d47a1f00a61b892934bb3a38c7d36d
Leonardo Ebert <leoebert19@gmail.com>
2024-05-28 21:32:03 -0300

    feat: added migration for fk between user and auth role

---
+++ src/database/migrations/1716942071685-CreateRoleColumnOnUserTable.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, TableColumn, TableForeignKey } from 'typeorm';
+import { query } from 'express';
+
+export class CreateRoleColumnOnUserTable1716942071685 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn('user', new TableColumn(
+      {
+        name: 'role_id',
+        type: 'uuid',
+        isNullable: false,
+      }
+    ));
+
+    await queryRunner.createForeignKey('user',
+      new TableForeignKey({
+        columnNames: ['role_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'auth_role',
+        onDelete: 'CASCADE'
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('user');
+    const foreignKey = table.foreignKeys.find(fk => fk.columnNames.indexOf('role_id') !== -1);
+    await queryRunner.dropForeignKey('user', foreignKey);
+    await queryRunner.dropColumn(table, 'role_id');
+  }
+
+}
