69339a47dd806a353edf8153dbc832b08fc5ac3c
Leonardo Ebert <leoebert19@gmail.com>
2024-06-04 22:02:17 -0300

    feat: added function for vendor classification list request

--- src/vendor/classification/classification.service.ts
+++ src/vendor/classification/classification.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateClassificationDto } from './dto/create-classification.dto';
 import { UpdateClassificationDto } from './dto/update-classification.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { VendorClassification } from '@/vendor/classification/entities/vendor-classification.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ClassificationService {
+  constructor(
+    @InjectRepository(VendorClassification)
+    private readonly vendorClassificationRepository: Repository<VendorClassification>,
+  ) {}
+
   create(createClassificationDto: CreateClassificationDto) {
     return 'This action adds a new classification';
   }
 
   findAll() {
-    return `This action returns all classification`;
+    return this.vendorClassificationRepository.find();
   }
 
   findOne(id: number) {
