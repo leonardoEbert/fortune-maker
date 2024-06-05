03e61e6cff9eae25192ecfd3dfa0b07551edbb5c
Leonardo Ebert <leoebert19@gmail.com>
2024-06-04 22:00:34 -0300

    feat: implemented typeorm forFeature for vendor classification on vendor classification module

--- src/vendor/classification/classification.module.ts
+++ src/vendor/classification/classification.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ClassificationService } from './classification.service';
 import { ClassificationController } from './classification.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VendorClassification } from '@/vendor/classification/entities/vendor-classification.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([VendorClassification])],
   controllers: [ClassificationController],
   providers: [ClassificationService],
 })
