ab55a8246fb6223969c5a8f5d04351655bdbfba8
Leonardo Ebert <leoebert19@gmail.com>
2024-06-19 10:12:20 -0300

    feat: added dto for vendor creation endpoint

--- src/vendor/dto/create-vendor.dto.ts
+++ src/vendor/dto/create-vendor.dto.ts
@@ -1,1 +1,8 @@
-export class CreateVendorDto {}
+import { VendorClassification } from '@/vendor/classification/entities/vendor-classification.entity';
+
+export class CreateVendorDto {
+  name: string;
+  description: string;
+  isActive: boolean;
+  classifications?: VendorClassification[];
+}
